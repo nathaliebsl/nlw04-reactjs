80372f160b754bc66f9289c0f0c9c990b313efd2
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-02-23 17:11:39 -0300

    add funcionalidade para o countdown

--- getmoovin/getmoovin-next/src/components/Countdown.tsx
+++ getmoovin/getmoovin-next/src/components/Countdown.tsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "../styles/components/Countdown.module.css";
 
 export function Countdown() {
   const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
 
   return (
     <div>
@@ -23,7 +36,11 @@ export function Countdown() {
           <span>{secondRight}</span>
         </div>
       </div>
-      <button className={styles.countdownButton} type="button">
+      <button
+        onClick={startCountdown}
+        className={styles.countdownButton}
+        type="button"
+      >
         Iniciar um ciclo
       </button>
     </div>
