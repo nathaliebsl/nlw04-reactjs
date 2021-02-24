b4fedff2b923b7ce4bd8dbca887ee87865650f4c
nathaliebsl <75685745+nathaliebsl@users.noreply.github.com>
2021-02-24 15:04:10 -0300

    add ChallengeBox e CSS

---
+++ getmoovin/getmoovin-next/src/components/ChallengeBox.tsx
@@ -0,0 +1,23 @@
+import styles from "../styles/components/ChallengeBox.module.css";
+
+export function ChallengeBox() {
+  const hasActiveChallenge = true;
+
+  return (
+    <div className={styles.challengeBoxContainer}>
+      {hasActiveChallenge ? (
+        <div className={styles.challengeActive}>
+
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>Finalize um ciclo para receber um desafio</strong>
+          <p>
+            <img src="/icons/level-up.svg" alt="Level Up" />
+            Suba de Level completando os desafios
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
--- getmoovin/getmoovin-next/src/pages/index.tsx
+++ getmoovin/getmoovin-next/src/pages/index.tsx
@@ -4,6 +4,7 @@ import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
 import Head from 'next/head';
+import { ChallengeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -14,12 +15,13 @@ export default function Home() {
       <ExperienceBar />
 
       <section>
-        <div>
+        <div className={styles.firstDiv}>
           <Profile />
           <CompletedChallenges />
           <Countdown />
         </div>
         <div>
+        <ChallengeBox />
         </div>
       </section>
     </div>
